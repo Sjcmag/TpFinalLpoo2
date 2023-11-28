8eb33765fff235968a1f8b72f76ac14b738ef8c8
Santiago Javier <santicolque5@gmail.com>
2023-11-28 16:04:54 -0300

    TP

---
+++ ClasesBase/TipoVehiculo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClasesBase
+{
+    public class TipoVehiculo
+    {
+        private int tipoV_Codigo;
+
+        public int TipoV_Codigo
+        {
+            get { return tipoV_Codigo; }
+            set { tipoV_Codigo = value; }
+        }
+        private string tipoV_Descripcion;
+
+        public string TipoV_Descripcion
+        {
+            get { return tipoV_Descripcion; }
+            set { tipoV_Descripcion = value; }
+        }
+        private decimal tipoV_Tarifa;
+
+        public decimal TipoV_Tarifa
+        {
+            get { return tipoV_Tarifa; }
+            set { tipoV_Tarifa = value; }
+        }
+
+        private string tipoV_Imagen;
+
+        public string TipoV_Imagen
+        {
+            get { return tipoV_Imagen; }
+            set { tipoV_Imagen = value; }
+        }
+    }
+}
